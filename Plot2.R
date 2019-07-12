05cbf8c53bfeedb317de426eae27ab73ee631848
drmuu <leo@leocarlsson.se>
2019-07-12 17:41:12 +0200

    Finished

--- Plot2.R
+++ Plot2.R
@@ -1,1 +1,31 @@
-#this is plot 2
+
+# setup and file load -----------------------------------------------------
+library(tidyverse)
+library(lubridate)
+
+#checks if data-folder exists, if not, create it
+if(!file.exists("data")){dir.create("data")} 
+
+#check if file is already downloaded and extracted the file, if not, do and clean
+if(!file.exists("./data/household_power_consumption.txt")) { 
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, "./Data/Power.zip")
+        unzip("./data/Power.zip", exdir = "./data")
+        file.remove("./Data/Power.zip")
+        rm(fileURL)
+}
+
+#reads it into a tibble
+power <- read_delim("./data/household_power_consumption.txt", delim = ";", na = "?")
+
+#convert dates from character strings, using lubridate
+power$Date <- dmy(power$Date)
+
+#filter out the days we wanna plot
+power <- filter(power, Date ==  "2007-02-01" | Date ==  "2007-02-02")
+
+# plot --------------------------------------------------------------------
+#Here I fail to plot the weekday names on the x-axis. Please give hints if you managed! 
+png(filename = "Plot2.png", width = 480, height = 480)
+plot(power$Global_active_power, xaxt = "n", type =  "l", ylab = "Global Active Power (kilowatts)")
+dev.off()
--- Plot4.R
+++ Plot4.R
@@ -1,1 +1,46 @@
-#this is plot 4
+
+# setup and file load -----------------------------------------------------
+library(tidyverse)
+library(lubridate)
+
+#checks if data-folder exists, if not, create it
+if(!file.exists("data")){dir.create("data")} 
+
+#check if file is already downloaded and extracted the file, if not, do and clean
+if(!file.exists("./data/household_power_consumption.txt")) { 
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, "./Data/Power.zip")
+        unzip("./data/Power.zip", exdir = "./data")
+        file.remove("./Data/Power.zip")
+        rm(fileURL)
+}
+
+#reads it into a tibble
+power <- read_delim("./data/household_power_consumption.txt", delim = ";", na = "?")
+
+#convert dates from character strings, using lubridate
+power$Date <- dmy(power$Date)
+
+#filter out the days we wanna plot
+power <- filter(power, Date ==  "2007-02-01" | Date ==  "2007-02-02")
+
+# plot --------------------------------------------------------------------
+png(filename = "Plot4.png", width = 480, height = 480)
+par(mfrow=c(2,2))
+#topleft
+plot(power$Global_active_power, xaxt = "n", type =  "l", ylab = "Global Active Power")
+
+#topright
+plot(power$Voltage, xaxt = "n", type =  "l", ylab = "Voltage")
+
+#bottomleft
+plot(power$Sub_metering_1, xaxt = "n", type =  "n", ylab = "Energy sub metering")
+lines(power$Sub_metering_1, col="black")
+lines(power$Sub_metering_2, col="red")
+lines(power$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1, col=c("black", "red", "blue"))
+
+#bottomright
+plot(power$Global_reactive_power, xaxt = "n", type =  "l", ylab = "Global Reactive Power")
+
+dev.off()
